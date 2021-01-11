6c5f897571a8a843e31b62132cbbf84454a995ae
pan duo la <1721714951@qq.com>
2021-01-11 17:50:09 +0800

    oneday

---
+++ src/main/java/com/ran/Test.java
@@ -0,0 +1,11 @@
+package com.ran;
+
+/**
+ * @author pan duo la
+ * @date 2021/1/11 15:49
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hahah");
+    }
+}
